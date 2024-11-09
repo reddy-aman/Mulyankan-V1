0a65a378ec43b6577d70580cf7b1c8443e7b653b
Aman Reddy <reddyaman@sameer.ac.in>
2024-11-10 00:39:49 +0530

    change

--- database/seeders/CoursesdetailsTableSeeder.php
+++ database/seeders/CoursesdetailsTableSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class AttributesTableSeeder extends Seeder
+class CoursesdetailsTableSeeder extends Seeder
 {
     public function run()
     {
