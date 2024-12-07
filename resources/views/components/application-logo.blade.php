ce0bcda80b4061d1b08cbdd2e8f35367191ceda7
Aman Reddy <reddyaman10@gmail.com>
2024-12-06 20:23:42 -0800

    landing page updated

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,21 +1,28 @@
 <style>
+/* Center the logo for all screen sizes */
+.responsive-logo-container {
+  display: flex; /* Enable flexbox layout */
+  justify-content: center; /* Center horizontally */
+  align-items: center; /* Optional: Center vertically */
+  margin-bottom: 20px;;
+
+}
 
 .responsive-logo {
   max-width: 100%; /* Make the logo responsive */
   height: auto; /* Maintain aspect ratio */
-  width: 50px; /* Set a base width for larger screens */
+  width: 65px; /* Set a base width for larger screens */
 }
 
 /* Optional: Adjust for smaller screens */
 @media (max-width: 500px) {
   .responsive-logo {
-    width: 35px; /* Adjust width for smaller screens */
-   
+    width: 55px; /* Adjust width for smaller screens */
+    margin-bottom: 20px;;
   }
 }
-
 </style>
 
-<div class="responsive-logo">
+<div class="responsive-logo-container">
   <img src="{{ asset('images/IITB.png') }}" alt="IITB Logo" class="responsive-logo">
 </div>
